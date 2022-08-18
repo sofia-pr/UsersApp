4ae71a3bfaf37a411f03283c71a2b34b1792a8e9
Sofia Punal <sofia.punal@gmail.com>
2022-08-18 18:10:40 +0200

    updates

---
+++ src/components/UI/Button.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./Button.module.css";
+
+const Button = (props) => {
+  return (
+    <button
+      onClick={props.onClick}
+      type={props.type || "button"}
+      className={classes.button}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
